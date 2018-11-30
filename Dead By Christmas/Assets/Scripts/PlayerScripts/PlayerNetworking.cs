7edc4501bfd395f254be2360596634f2554b1007
Thimo Reumerman <33859063+ThimoReumerman@users.noreply.github.com>
2018-11-30 13:33:44 +0100

    Added comments

--- Dead By Christmas/Assets/Scripts/PlayerScripts/PlayerNetworking.cs
+++ Dead By Christmas/Assets/Scripts/PlayerScripts/PlayerNetworking.cs
@@ -3,36 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerNetworking : MonoBehaviour {
-	Vector3 position;
-	[SerializeField] float smoothing;
-	PhotonView photonView;
+	Vector3 position; //Player pos for the network
+	Quaternion rotation; //Player rotation for the network
+	[SerializeField] float smoothing; //Lerp smoothing of position and rotation
+	PhotonView photonView; //PhotonView of player
 	
 	void Start () {
-		photonView = GetComponent<PhotonView>();
-		if (photonView.isMine) {
+		photonView = GetComponent<PhotonView>(); //Get the PhotonView of the player
+
+		//Check if the PhotonView is yours
+		if (photonView.isMine) { 
 			print("Is mine...");
-			GetComponent<Player> ().enabled = true;
+			GetComponent<Player> ().enabled = true; //Enable player behaviour script
 
 		} else {
-			print("Is not mine...");
-			StartCoroutine ("UpdateData");
+			StartCoroutine ("UpdateData"); //Update the data of the player
 		}
 	}
 
 	IEnumerator UpdateData () {
 		while (true) {
 			transform.position = Vector3.Lerp (transform.position, position, Time.deltaTime * smoothing);
-
+			transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * smoothing);
 			yield return null;
 		}
 	}
 
 	void OnPhotonSerializeView (PhotonStream stream, PhotonMessageInfo info) {
+		//Check if you are writing to the network
 		if (stream.isWriting) {
 			stream.SendNext (transform.position);
+			stream.SendNext(transform.rotation);
 		} else {
-
 			position = (Vector3) stream.ReceiveNext ();
+			rotation = (Quaternion) stream.ReceiveNext();
 
 		}
 	}
