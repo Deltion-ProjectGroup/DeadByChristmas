6d492fd004e8d41ff26719ae4c554f442a726845
JelmerBatenburg <33858985+JelmerBatenburg@users.noreply.github.com>
2018-12-03 22:27:45 +0100

    pueashea
    added the crafting fillbar (can get some refinement (mostly UI update)).
    intergrated the basic player movement(kinda clamp doesnt work).
    
    ZA

--- Dead By Christmas/Assets/Scripts/GameEssential/ElfController.cs
+++ Dead By Christmas/Assets/Scripts/GameEssential/ElfController.cs
@@ -1,34 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ElfController : MonoBehaviour {
+public class ElfController : Player {
 
 	public enum StruggleState { normal, struggling, knockedOut , Crafting}
+    [Header("CurrentState")]
     public StruggleState struggleState;
+
     [Header("StruggleInfo")]
     public string struggleInput;
     public float struggleMoveSpeed;
     public int struggleTime;
     public float pullBackSpeed;
     int struggling;
+
     [Header("CraftingInfo")]
     public float itemDetectRange;
     public LayerMask craftingItemsMask;
-    private bool canCraft;
+    bool canCraft;
     public int minimumItemAmount;
     public string craftingInput;
     public int craftingTime;
-    private IEnumerator currentCrafting;
+    IEnumerator currentCrafting;
+    public float camBackwardsDistance;
+
     [Header("KnockOutInfo")]
     public Rigidbody[] bones;
     public bool isKnockedOut = true;
 
+    [Header("ExtraCharacterInfo")]
+    public GameObject fillBar;
+    GameObject currentFillbar;
+    [SerializeField] Transform currentCam;
+
+    //StartFunction
+    public void Start()
+    {
+        PlayerStart();
+    }
+
+    //Update Function
     public void Update()
     {
         CheckState();
     }
 
+    //Checkes the state the elf is in
     public void CheckState()
     {
         switch (struggleState)
@@ -50,29 +69,11 @@ public class ElfController : MonoBehaviour {
         }
     }
 
-    public void CheckForItems()
-    {
-        Collider[] itemsInRange = Physics.OverlapSphere(transform.position, itemDetectRange, craftingItemsMask);
-        canCraft = (itemsInRange.Length >= minimumItemAmount)? true : false;
-    }
-
-    public IEnumerator StartCrafting(float time)
-    {
-        for (float i = 0; i < time; i += 0.1f)
-        {
-            yield return new WaitForSeconds(0.1f);
-        }
-
-        Collider[] itemsInRange = Physics.OverlapSphere(transform.position, itemDetectRange, craftingItemsMask);
-        for (int i = 0; i < minimumItemAmount; i++)
-        {
-            Destroy(itemsInRange[i].gameObject);
-        }
-        struggleState = StruggleState.normal;
-    }
-
+    //The normal state
     public void Normal()
     {
+        PlayerFixedUpdate();
+        PlayerUpdate();
         if (isKnockedOut)
             ToggleRagdoll(false);
         CheckForItems();
@@ -80,27 +81,17 @@ public class ElfController : MonoBehaviour {
         {
             currentCrafting = StartCrafting(craftingTime);
             StartCoroutine(currentCrafting);
-            struggleState = StruggleState.Crafting;
         }
     }
 
-    public void ToggleRagdoll(bool onOrOf)
+    //Checks if you are able to craft a weapon
+    public void CheckForItems()
     {
-        isKnockedOut = onOrOf;
-        GetComponent<Animator>().enabled = !onOrOf;
-        foreach (Rigidbody joint in bones)
-        {
-            joint.isKinematic = !onOrOf;
-        }
-        if (onOrOf)
-        {
-            foreach (Rigidbody joint in bones)
-            {
-                joint.AddExplosionForce(1000,transform.position + transform.forward + Vector3.up * 0.5f,30);
-            }
-        }
+        Collider[] itemsInRange = Physics.OverlapSphere(transform.position, itemDetectRange, craftingItemsMask);
+        canCraft = (itemsInRange.Length >= minimumItemAmount)? true : false;
     }
 
+    //The struggle state
     public void Struggling()
     {
         if (isKnockedOut)
@@ -119,21 +110,76 @@ public class ElfController : MonoBehaviour {
         transform.Translate(-Vector3.forward * Time.deltaTime * pullBackSpeed);
     }
 
+    //the knocked out state
     public void KnockedOut()
     {
         if (!isKnockedOut)
             ToggleRagdoll(true);
     }
 
+    //Toggle the ragdoll
+    public void ToggleRagdoll(bool onOrOf)
+    {
+        isKnockedOut = onOrOf;
+        GetComponent<Animator>().enabled = !onOrOf;
+        foreach (Rigidbody joint in bones)
+        {
+            joint.isKinematic = !onOrOf;
+        }
+        if (onOrOf)
+        {
+            foreach (Rigidbody joint in bones)
+            {
+                joint.AddExplosionForce(1000,transform.position + transform.forward + Vector3.up * 0.5f,30);
+            }
+        }
+    }
+
+    //the crafting state
     public void Crafting()
     {
         if (Input.GetButtonUp(craftingInput))
         {
             struggleState = StruggleState.normal;
             StopCoroutine(currentCrafting);
+            currentCam.position += currentCam.forward * camBackwardsDistance;
+            Destroy(currentFillbar);
         }
     }
 
+    //Call this to start crafting.
+    public IEnumerator StartCrafting(float time)
+    {
+        currentFillbar = Instantiate(fillBar, transform.position + -currentCam.forward, Quaternion.identity);
+        currentCam.position -= currentCam.forward * camBackwardsDistance;
+        currentFillbar.transform.LookAt(currentCam);
+        struggleState = StruggleState.Crafting;
+        FillbarValueSet fillBarComponent = currentFillbar.GetComponent<FillbarValueSet>();
+        //sets the fillbar value.
+        for (float i = 0; i < time; i += 0.05f)
+        {
+            fillBarComponent.fillbar.fillAmount = 1f / time * i;
+            yield return new WaitForSeconds(0.05f);
+        }
+        //destroys the items in range.
+        Collider[] itemsInRange = Physics.OverlapSphere(transform.position, itemDetectRange, craftingItemsMask);
+        for (int i = 0; i < minimumItemAmount; i++)
+        {
+            Destroy(itemsInRange[i].gameObject);
+        }
+        struggleState = StruggleState.normal;
+        currentCam.position += currentCam.forward * camBackwardsDistance;
+        Destroy(currentFillbar);
+        Crafted();
+    }
+
+    //This is called when youre done crafting an item.
+    public void Crafted()
+    {
+        Debug.Log("Crafted Weapon");
+    }
+
+    //gizmos
     public void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.yellow;
