7f210889d0785f344ad6c2d15445ffb2cd6b4bcb
MauritsHeetkamp <mauritsmupsketje@gmail.com>
2018-12-14 13:25:53 +0100

    Fix
    TransitionFix

--- Dead By Christmas/Assets/Scripts/BasicUIScripts/TransitionScreen.cs
+++ Dead By Christmas/Assets/Scripts/BasicUIScripts/TransitionScreen.cs
@@ -4,26 +4,25 @@ using UnityEngine;
 
 public class TransitionScreen : MonoBehaviour {
     public static TransitionScreen transitionScreen;
-    GameObject screen;
+    [SerializeField]GameObject screen;
 
     public void Awake()
     {
         transitionScreen = this;
-        screen = gameObject;
     }
     //The fadein animation
     [PunRPC]
     public IEnumerator FadeIn()
     {
         screen.SetActive(true);
-        GetComponent<Animation>().Play("TransitionFadeIn");
+        screen.GetComponent<Animation>().Play("TransitionFadeIn");
         yield return new WaitForSeconds(GetComponent<Animation>().GetClip("TransitionFadeIn").length);
     }
     //The fadeout animation
     [PunRPC]
     public IEnumerator FadeOut()
     {
-        GetComponent<Animation>().Play("TransitionFadeOut");
+        screen.GetComponent<Animation>().Play("TransitionFadeOut");
         yield return new WaitForSeconds(GetComponent<Animation>().GetClip("TransitionFadeOut").length);
         screen.SetActive(false);
     }
