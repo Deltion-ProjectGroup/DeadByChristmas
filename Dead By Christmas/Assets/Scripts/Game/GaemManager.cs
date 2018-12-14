3df5cc7673266a959702bcce022adc8b4a90df12
MauritsHeetkamp <mauritsmupsketje@gmail.com>
2018-12-14 12:38:47 +0100

    Timer Update
    There is a timer now? WOW~!

--- Dead By Christmas/Assets/Scripts/Game/GaemManager.cs
+++ Dead By Christmas/Assets/Scripts/Game/GaemManager.cs
@@ -16,14 +16,21 @@ public class GaemManager : MonoBehaviour {
     public Transform[] santaSpawns, elfSpawns, weaponPartSpots;
     [Range(3, 15)]
     public int partAmount;
+    public Text timerText;
+    int remainingSeconds;
+    [SerializeField] int roundTime;
 	// Use this for initialization
+    //Spawns weapons and showsRoles
 	void Start () {
         if (PhotonNetwork.isMasterClient)
         {
+            remainingSeconds = roundTime;
+            timerText.text = CalcRemainingTime();
             GetComponent<PhotonView>().RPC("ShowRoles", PhotonTargets.All);
             GetComponent<PhotonView>().RPC("SpawnWeaponParts", PhotonTargets.MasterClient);
         }
 	}
+    //Randomizes the roles
     [PunRPC]
     public void RandomizePlayers()
     {
@@ -35,7 +42,7 @@ public class GaemManager : MonoBehaviour {
         }
         isSanta[allPlayers[Random.Range(0, allPlayers.Length)].NickName] = true;
     }
-
+    //shows the roles
     [PunRPC]
     public IEnumerator ShowRoles()
     {
@@ -62,6 +69,7 @@ public class GaemManager : MonoBehaviour {
             TransitionScreen.transitionScreen.FadeOut();
         }
     }
+    //spawns the weapon parts
     [PunRPC]
     public void SpawnWeaponParts()
     {
@@ -78,8 +86,23 @@ public class GaemManager : MonoBehaviour {
         }
 
     }
+    IEnumerator Timer()
+    {
+        while(remainingSeconds > 0)
+        {
+            yield return new WaitForSeconds(1);
+            remainingSeconds--;
+            CalcRemainingTime();
+        }
+        timerText.text = "GAME DONE";
+    }
+    //spawns the player
     public void SpawnPlayer()
     {
+        if (PhotonNetwork.isMasterClient)
+        {
+            StartCoroutine(Timer());
+        }
         if((bool)isSanta[PhotonNetwork.player.NickName] == true)
         {
             localPlayer = PhotonNetwork.Instantiate(santaPrefab, santaSpawns[Random.Range(0, santaSpawns.Length)].position, Quaternion.identity, 0);
@@ -89,16 +112,32 @@ public class GaemManager : MonoBehaviour {
             localPlayer = PhotonNetwork.Instantiate(elfPrefab, elfSpawns[Random.Range(0, elfSpawns.Length)].position, Quaternion.identity, 0);
         }
     }
-
+    public void OnMasterClientSwitched()
+    {
+        if (PhotonNetwork.isMasterClient)
+        {
+            StartCoroutine(Timer());
+        }
+    }
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
         if (stream.isWriting)
         {
             stream.SendNext(isSanta);
+            stream.SendNext(remainingSeconds);
+            stream.SendNext(timerText.text);
         }
         else
         {
             isSanta = (ExitGames.Client.Photon.Hashtable)stream.ReceiveNext();
+            remainingSeconds = (int)stream.ReceiveNext();
+            timerText.text = (string)stream.ReceiveNext();
         }
+    }
+    string CalcRemainingTime()
+    {
+        int minutes = Mathf.FloorToInt(remainingSeconds / 60);
+        int seconds = ((remainingSeconds / 60) - minutes) * 60;
+        return minutes.ToString() + ":" + seconds.ToString();
     }
 }
