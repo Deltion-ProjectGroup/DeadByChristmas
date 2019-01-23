a4d3d5b6620acffb7c8f4100385301611223389a
JelmerBatenburg <33858985+JelmerBatenburg@users.noreply.github.com>
2019-01-23 22:54:55 +0100

    ULikeJazz
    added music

---
+++ Dead By Christmas/Assets/Scripts/Other/BackGroundMusic.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class BackGroundMusic : MonoBehaviour {
+    public AudioClipInfo[] songs;
+    public string objectTag;
+    [HideInInspector]
+    public bool indestructible;
+    public AudioSource source;
+
+    public void Awake()
+    {
+        GameObject[] musicPlayers = GameObject.FindGameObjectsWithTag(objectTag);
+        if(musicPlayers.Length != 1)
+        {
+            foreach (GameObject obj in musicPlayers)
+                if (!obj.GetComponent<BackGroundMusic>().indestructible)
+                    Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+            indestructible = true;
+            StartCoroutine(PlayMusic());
+        }
+    }
+
+    public IEnumerator PlayMusic()
+    {
+        int index = Random.Range(0, songs.Length);
+        source.PlayOneShot(songs[index].song);
+        yield return new WaitForSeconds(songs[index].time);
+        StartCoroutine(PlayMusic());
+    }
+}
+
+[System.Serializable]
+public class AudioClipInfo
+{
+    public AudioClip song;
+    public float time;
+}
